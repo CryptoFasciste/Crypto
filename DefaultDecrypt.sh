6b89c2bae63d8a3ad2870403103b44be7f64279c
CryptoFasciste <85704454+CryptoFasciste@users.noreply.github.com>
2021-06-11 01:22:55 +0200

    Update DefaultDecrypt.sh

--- DefaultDecrypt.sh
+++ DefaultDecrypt.sh
@@ -1,6 +1,8 @@
 # $1 message to decrypt
 # $2 out.txt
 
+mkdir contacts 2> /dev/null
+
 MyIdentity=$(cat util/default_identity.txt)
 MyPrivKey=identities/$MyIdentity/"$MyIdentity"_privkey.pem
 ./util/decrypt.sh $MyPrivKey $1 $2
