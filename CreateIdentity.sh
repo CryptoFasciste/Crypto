936d5325311da2dd451493cf90c1b89f78dc24f2
CryptoFasciste <85704454+CryptoFasciste@users.noreply.github.com>
2021-06-11 01:20:13 +0200

    Update CreateIdentity.sh

--- CreateIdentity.sh
+++ CreateIdentity.sh
@@ -10,7 +10,7 @@ openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen
 openssl pkey -in identities/$1/$1_privkey.pem -out identities/$1/$1_pubkey.pem -pubout
 
 # Add user as default
-read -p "Set as default user [y/n]: " add_user
+read -p "Set as default user [y/N]: " add_user
 if [ "$add_user" == "y" ] || [ "$add_user" == "yes" ];
 then
 	echo $1 > util/default_identity.txt
